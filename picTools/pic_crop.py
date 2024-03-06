1bebc47c1f10ebe76cc3445758a678041ea2fe2b
hawor <hongkang.luan@antalpha.com>
2024-03-06 11:28:55 +0800

    从测试数据集中裁剪数据

--- picTools/pic_crop.py
+++ picTools/pic_crop.py
@@ -5,27 +5,53 @@ import h5py
 
 def get_crop_img_from_dataset(data_root, patch_size):
     bgrs = []
+    hypers = []
     bgr_data_path = data_root + '\\Valid_RGB\\'
+    hyper_data_path = data_root + '\\Valid_spectral\\'
     valid_list_path = data_root + '\\split_txt\\valid_list.txt'
 
     with open(valid_list_path, 'r') as fin:
         bgr_list = [line.replace('\n', '.jpg') for line in fin]
+        hyper_list = [line.replace('jpg', 'mat') for line in bgr_list]
     bgr_list.sort()
-    h, w = 482, 512
-    patch_per_line = (w - patch_size) // 8 + 1
-    patch_per_col = (h - patch_size) // 8 + 1
+    hyper_list.sort()
+
+    patch_per_line = 4
+    patch_per_col = 3
     patch_per_img = patch_per_line * patch_per_col
 
     for i in range(len(bgr_list)):
         bgr_path = bgr_data_path + bgr_list[i]
         bgr = cv2.imread(bgr_path)
-        bgrs.append(bgr)
+        # bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+        # bgr = np.transpose(bgr, [2, 0, 1])
+        bgrs.append(bgr)    # 482, 512, 3
 
-    for i in range(50):
-        img_idx, patch_idx = i // patch_per_img, i % patch_per_img
-        h_idx, w_idx = patch_idx // patch_per_line, patch_idx % patch_per_line
-        crop_bgr = bgrs[img_idx][h_idx * 8:h_idx * 8 + patch_size, w_idx * 8:w_idx * 8 + patch_size, :]
-        cv2.imwrite(f'D:\Datasets\\rgbs\crop_bgr_{i}.jpg', crop_bgr)
+        hyper_path = hyper_data_path + hyper_list[i]
+        with h5py.File(hyper_path, 'r') as mat:
+            hyper = np.float32(np.array(mat['cube']))   # 31, 512, 482
+        # hyper = np.transpose(hyper, [0, 2, 1])
+        hypers.append(hyper)
+
+    for idx in range(120):
+        img_idx = idx // patch_per_img
+        patch_idx = idx % patch_per_img
+
+        bgr = bgrs[img_idx]
+        hyper = hypers[img_idx]
+
+        h = patch_idx // patch_per_line
+        w = patch_idx % patch_per_line
+
+        # bgr = bgr[:, h * 128:h * 128 + patch_size, w * 128:w * 128 + patch_size]
+        # hyper = hyper[:, h * 128:h * 128 + patch_size, w * 128:w * 128 + patch_size]
+        bgr = bgr[h * 128:h * 128 + patch_size, w * 128:w * 128 + patch_size, :]
+        hyper = hyper[:, w * 128:w * 128 + patch_size, h * 128:h * 128 + patch_size]
+
+        cv2.imwrite(f'D:\Codes\Tools\output\RGBs\crop_bgr_{idx}.jpg', bgr)
+        with h5py.File(f'D:\Codes\Tools\output\HSIs\crop_hyper_{idx}.mat', 'w') as f:
+            f.create_dataset('cube', data=hyper)
+        print(f'crop_{idx} successfully!')
 
 
 def get_crop_img_from_single_img(data_root, patch_size):
@@ -47,7 +73,7 @@ def get_crop_mat_from_single_mat(data_root, patch_size):
     # (31, 512, 482)
     with h5py.File(data_root, 'r') as f:
         hyper = np.float32(np.array(f['cube']))
-    _, h, w = hyper.shape
+    _, w, h = hyper.shape
     hyper = np.transpose(hyper, [2, 1, 0])
     patch_per_line = (w - patch_size) // 128 + 1
     patch_per_col = (h - patch_size) // 128 + 1
@@ -64,6 +90,6 @@ def get_crop_mat_from_single_mat(data_root, patch_size):
 
 
 if __name__ == '__main__':
-    # get_crop_img_from_dataset(r'D:\Datasets\data2022', 128)
+    get_crop_img_from_dataset(r'D:\Datasets\data2022test\\', 128)
     # get_crop_img_from_single_img(r'D:\Datasets\data2022\ARAD_1K_0914.jpg', 128)
-    get_crop_mat_from_single_mat(r'D:\Datasets\data2022\ARAD_1K_0914.mat', 128)
+    # get_crop_mat_from_single_mat(r'D:\Datasets\data2022\ARAD_1K_0914.mat', 128)
