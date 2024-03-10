347050f1ec2fb2f2c2d87c7349baf7ab3c6b8f04
Lassi-Ki <913876018@qq.com>
2024-03-10 17:14:37 +0800

    增加误差图功能

--- visualize/error_map.py
+++ visualize/error_map.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 
 sorted_img_list = [
-    # r"D:\Codes\Tools\output\splits\HSCNN_plus_split",
-    # r"D:\Codes\Tools\output\splits\AWAN_split",
-    # r"D:\Codes\Tools\output\splits\HRNet_split",
-    # r"D:\Codes\Tools\output\splits\MST_split",
-    # r"D:\Codes\Tools\output\splits\MS3T_split",
-    r"D:\Codes\Tools\output\splits\mst_plus_plus_split",
-    r"D:\Codes\Tools\output\splits\gdlnet_split",
+    r"D:\Codes\Tools\output\out_splits\hscnn_plus_split",
+    r"D:\Codes\Tools\output\out_splits\awan_split",
+    r"D:\Codes\Tools\output\out_splits\hrnet_split",
+    r"D:\Codes\Tools\output\out_splits\mst_split",
+    r"D:\codes\Tools\output\out_splits\ms3t_split",
+    r"D:\codes\Tools\output\out_splits\mst_plus_plus_split",
+    r"D:\codes\Tools\output\out_splits\gdlnet_split",
 ]
 
 
@@ -78,16 +78,49 @@ def make_group_error_map(gen_folder, real_img_path, patch, channel, save_path=No
 
 
 def traverse():
-    for patch_th in range(0, 120):
+    for patch_th in range(96, 120):
         for channel_th in range(0, 31):
             make_group_error_map("",
-                                 r"D:\Codes\Tools\output\HSIs_split",
+                                 r"D:\codes\Tools\output\HSIs_split",
                                  patch_th, channel_th,
                                  r"D:\Codes\Tools\output\error_map")
-            print(f'patch: {patch_th}, channel: {channel_th} sueecssfully!')
+            # print(f'patch: {patch_th}, channel: {channel_th} sueecssfully!')
+        print(f'patch: {patch_th} sueecssfully!')
+        if patch_th == 107:
+            break
+
+
+def make_single_error_map(real_img_path, patch, channel, save_path=None):
+    save_path = os.path.join(save_path, f'patch_{patch}_channel_{channel}')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    gen_folder_list = sorted_img_list
+    _gen_folder_list = []
+
+    for path in gen_folder_list:
+        gen_img_path = path
+        gen_img_path = os.path.join(gen_img_path,
+                                    f'patch_crop_bgr_{patch}',
+                                    f'crop_bgr_{patch}_channel_{channel}.jpg')
+        _gen_folder_list.append(gen_img_path)
+    gen_folder_list = _gen_folder_list
+
+    real_img_path = os.path.join(real_img_path,
+                                 f'patch_crop_hyper_{patch}',
+                                 f'crop_hyper_{patch}_channel_{channel}.jpg')
+    base_image = cv2.imread(real_img_path, cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite(os.path.join(save_path, 'real.jpg'), base_image)
+
+    for idx, current_img_path in enumerate(gen_folder_list):
+        current_model = current_img_path.split('\\')[-3].rsplit('_split')[0]
+        current_img = cv2.imread(current_img_path, cv2.IMREAD_GRAYSCALE)
+        diff_image = np.abs(current_img.astype(int) - base_image.astype(int)).astype(np.uint8)
+        normalized_gray_image = cv2.normalize(diff_image, None, 0, 255, cv2.NORM_MINMAX)
+        color_img = cv2.applyColorMap(normalized_gray_image, cv2.COLORMAP_JET)
+        cv2.imwrite(os.path.join(save_path, f'{current_model}.jpg'), color_img)
 
 
 if __name__ == "__main__":
-    # make_error_map(r'D:\Codes\Tools\output\mst_plus_plus_split\patch_crop_bgr_105\crop_bgr_105_channel_15.jpg',
-    #                r'D:\Codes\Tools\output\HSIs_split\patch_crop_hyper_105\crop_hyper_105_channel_15.jpg')
-    traverse()
+    # traverse()
+    make_single_error_map(r"D:\codes\Tools\output\HSIs_split", 104, 0, r"D:\Codes\Tools\output\error_map")
